68e61e64e9a95aff58c933d3e4573b0fbd93951e
SKATASHIN <katashin0544@gmail.com>
2022-08-30 16:12:42 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+    <x-slot name="header">ヘッダー</x-slot>
+    
   コンポーネントテスト１
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,5 @@
 <x-tests.app>
+  <x-slot name="header">ヘッダー</x-slot>
+  
   コンポーネントテスト2
 </x-tests.app>
