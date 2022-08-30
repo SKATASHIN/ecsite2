3cfd71483a8a953cb8295816ceb7d245ab059156
SKATASHIN <katashin0544@gmail.com>
2022-08-30 17:40:18 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,5 +1,10 @@
 <x-tests.app>
   <x-slot name="header">ヘッダー</x-slot>
-  
+
   コンポーネントテスト2
+  <x-test-class-base classBaseMessage="メッセージでふ" />
+  
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="メッセージでふ" defaultMessage="初期値から変更しています"/>
+
 </x-tests.app>
