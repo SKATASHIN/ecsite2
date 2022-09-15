5d91988c780170b769afc5d9a0d3d4905c7e1b07
SKATASHIN <katashin0544@gmail.com>
2022-09-15 15:18:17 +0900

    image delete

--- resources/views/owner/images/edit.blade.php
+++ resources/views/owner/images/edit.blade.php
@@ -17,7 +17,7 @@
 
                   <div class="p-2 w-1/2 mx-auto">
                         <div class="relative">
-                          <label for="title" class="leading-7 text-sm text-gray-600">画像</label>
+                          <label for="title" class="leading-7 text-sm text-gray-600">画像タイトル</label>
                           <input type="text" id="title" name="title" value="{{ $image->title }}" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                         </div>
                       </div>
@@ -35,8 +35,25 @@
                       </div>
                     </div>
                   </form>
+                  <form id="delete_{{$image->id}}" method="post" action="{{ route('owner.images.destroy', ['image' => $image->id ] )}}">
+                    @csrf
+                    @method('delete')
+                    <div class="md:px-4 py-3">
+                      <div class="p-2 w-full flex justify-around mt-32">
+                        <a href="#" data-id="{{ $image->id }}" onclick="deletePost(this)" class=" text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded " _msthash="1322126" _msttexthash="79859" _msthidden="1">削除する</a>
+                      </div>
+                  </div>
+                </form>
               </div>
           </div>
       </div>
   </div>
+  <script>
+    function deletePost(e) {
+        'use strict';
+        if (confirm('本当に削除してもいいですか?')) {
+        document.getElementById('delete_' + e.dataset.id).submit();
+        }
+    }
+    </script>
 </x-app-layout>
