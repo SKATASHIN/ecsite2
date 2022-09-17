8f1614d422c1ab3ddbe7d66a2922ca9bf20a8488
SKATASHIN <katashin0544@gmail.com>
2022-09-17 09:51:57 +0900

    EditUpdatecommit変更分

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -119,7 +119,7 @@ return [
     'attributes'           => [
         'name' => '名前',
         'email' => 'メールアドレス',
-        'password' => 'パスワード'
-        
+        'password' => 'パスワード',
+        'quantity' => '数量（在庫）'
     ],
 ];
