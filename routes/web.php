973681bf4b3caca0b65168d0adf6e60668e5c70a
SKATASHIN <katashin0544@gmail.com>
2022-09-19 14:33:18 +0900

    stripe controller

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 use App\Http\Controllers\User\ItemController;
 use App\Http\Controllers\User\CartController;
+use App\Models\Cart;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +31,7 @@ Route::prefix('cart')->middleware('auth:users')->group(function(){
         Route::get('/', [CartController::class, 'index'])->name('cart.index');
         Route::post('add', [CartController::class, 'add'])->name('cart.add');   
         Route::post('delete/{item}', [CartController::class, 'delete'])->name('cart.delete');
+        Route::get('checkout', [CartController::class,'checkout'])->name('cart.checkout');
 });
 
 // Route::get('/dashboard', function () {
