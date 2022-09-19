f13ec9cd9e25bc4481d28e9a1b4af315822288b5
SKATASHIN <katashin0544@gmail.com>
2022-09-19 10:22:03 +0900

    cart add

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ComponentTestController;
 use App\Http\Controllers\LifeCycleTestController;
 use App\Http\Controllers\User\ItemController;
+use App\Http\Controllers\User\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,12 +18,16 @@ use App\Http\Controllers\User\ItemController;
 */
 
 Route::get('/', function () {
-    return view('user.welcome');
+        return view('user.welcome');
 });
 
 Route::middleware('auth:users')->group(function(){
         Route::get('/', [ItemController::class, 'index'])->name('items.index');
         Route::get('show/{item}', [ItemController::class, 'show'])->name('items.show');
+});
+
+Route::prefix('cart')->middleware('auth:users')->group(function(){
+        Route::post('add', [CartController::class, 'add'])->name('cart.add');
 });
 
 // Route::get('/dashboard', function () {
